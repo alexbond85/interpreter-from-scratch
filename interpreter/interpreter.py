8c06f874b919d96574f83200372dc74061557812
Alexander Bondarenko <alexander.bondarenko@shippeo.com>
2022-07-16 18:07:01 +0200

    ast, visitor pattern

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -18,7 +18,7 @@ class NodeVisitor(object):
         visitor: Callable = getattr(self, method_name, self.generic_visit)
         return visitor(node)
 
-    def generic_visit(self, node):
+    def generic_visit(self, node) -> NoReturn:
         error_msg = f"No visit_{type(node).__name__} method"
         raise InterpreterError(error_msg)
 
@@ -36,14 +36,16 @@ class Interpreter(NodeVisitor):
         raise InterpreterError("Invalid syntax")
 
     def visit_BinOp(self, node: BinOp) -> int:
-        if node.op.type_ == TokenType.PLUS:
-            return self.visit(node.left) + self.visit(node.right)
-        elif node.op.type_ == TokenType.MINUS:
-            return self.visit(node.left) - self.visit(node.right)
-        elif node.op.type_ == TokenType.MUL:
-            return self.visit(node.left) * self.visit(node.right)
-        elif node.op.type_ == TokenType.DIV:
-            return self.visit(node.left) / self.visit(node.right)
+        ops_map = {
+            TokenType.PLUS: lambda x, y: x + y,
+            TokenType.MINUS: lambda x, y: x - y,
+            TokenType.MUL: lambda x, y: x * y,
+            TokenType.DIV: lambda x, y: x // y,
+        }
+        if node.op.type_ in ops_map:
+            value_left = self.visit(node.left)
+            value_right = self.visit(node.right)
+            return ops_map[node.op.type_](value_left, value_right)
         self._error()
 
     def visit_Num(self, node: Num) -> int:
